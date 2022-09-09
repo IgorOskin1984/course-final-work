1b09794539b63b6fa1317fbe7ad0ef26dc7dede6
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2022-09-09 10:58:00 +0300

    htm

--- js/my_js/index-page/link-home-page.js
+++ js/my_js/index-page/link-home-page.js
@@ -1,14 +1,7 @@
 //"use script";
 console.log(1);
-fetch("./html_pages/home_page.html")
-    .then(response => {
-        return response.text()
-    })
-    .then(data => {
-        document.querySelector("main").innerHTML = data;
-    });
 
-fetch("./html_pages/header_nav.html")
+fetch("./html_pages/header_nav.htm")
     .then(response => {
         return response.text()
     })
@@ -16,7 +9,15 @@ fetch("./html_pages/header_nav.html")
         document.querySelector('header').innerHTML = data;
     });
 
-fetch("./html_pages/footer.html")
+fetch("./html_pages/home_page.htm")
+    .then(response => {
+        return response.text()
+    })
+    .then(data => {
+        document.querySelector("main").innerHTML = data;
+    });
+
+fetch("./html_pages/footer.htm")
     .then(response => {
         return response.text()
     })
